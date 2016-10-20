ba8d2c3cd490389feee09ed70f9f0025d5bfd007
Bernhard Reiter <bernhard@intevation.de>
2016-10-20 15:46:54 +0200

    Dirty Setup: added mailgen config.

--- extras/dirty_setup.sh
+++ extras/dirty_setup.sh
@@ -52,8 +52,44 @@ sudo -u postgres bash -x << EOF
   fi
 EOF
 
+#
 # intelmq mailgen setup
-echo TODO
+# MUST adhere to /usr/share/doc/intelmq-mailgen/README.md.gz
+#
+maildbuser=intelmq_mailgen
+maildbpasswd=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c 14`
+
+sudo -u postgres bash -x << EOF
+  psql -c "CREATE USER $maildbuser WITH PASSWORD '$intelmqdbpasswd';"
+  psql -f /usr/share/intelmq-mailgen/sql/notifications.sql intelmq-events
+  psql -c "GRANT eventdb_insert TO $maildbuser" intelmq-events  
+  psql -c "GRANT eventdb_send_notifications TO $maildbuser" intelmq-events
+EOF
+
+cat /usr/share/doc/intelmq-mailgen/examples/intelmq-mailgen.conf.example | \
+  sed -e "s/your DB password/$maildbpasswd/" \
+      -e 's/"port": 25/"port": 8025/' \
+  > /etc/intelmq/intelmq-mailgen.conf
+
+ghome=/etc/intelmq/mailgen/gnupghome
+
+mkdir "$ghome"
+chown intelmq.intelmq "$ghome"
+sudo -u intelmq bash -x <<EOF
+  chmod og-rwx "$ghome"
+  GNUPGHOME="$ghome" \
+    gpg2 --import /usr/share/intelmq-mailgen/tests/keys/test1.sec
+
+  cat - >"$ghome/gpg.conf" <<FOF
+  personal-digest-preferences SHA256
+  no-emit-version
+  comment Key verification <https://example.org/hints-about-verification>
+FOF
+EOF
+
+cp /usr/share/doc/intelmq-mailgen/examples/example-template-dronereport.txt \
+   /etc/intelmq/mailgen/templates/template-Botnet-Drone-Hadoop.txt
+
 
 # intelmq overall setup
 echo TODO
