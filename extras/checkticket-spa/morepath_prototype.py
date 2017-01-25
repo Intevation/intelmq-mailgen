902431ec115c0947cf715ea7d5c84f07baf63038
Bernhard Reiter <bernhard@intevation.de>
2017-01-25 13:31:16 +0100

    checkticket-spa: improves morepath prototype.
     * adds serving the three main API calls which fody uses:
       /getLastTicketNumber /getEventIDsForTicket /getEventsForTicket

--- extras/checkticket-spa/morepath_prototype.py
+++ extras/checkticket-spa/morepath_prototype.py
@@ -26,10 +26,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Author(s):
     Bernhard E. Reiter <bernhard@intevation.de>
 """
+from datetime import datetime
 
+from psycopg2.extras import DictConnection
 import morepath
 
 import intelmqmail.cb as cb
+import intelmqmail.db as db
 
 class App(morepath.App):
     pass
@@ -38,6 +41,7 @@ class DBConnection():
     """Base class to keep the DB connection as class attribute.
 
     Will initialise the connection on first instanciation.
+    TODO: There is probably a better way to do this with morepath.
     """
     conn = None
     cur = None
@@ -47,7 +51,7 @@ class DBConnection():
             # initialising db connection
             config = cb.read_configuration()
             DBConnection.conn = cb.open_db_connection(
-                                    config, connection_factory=None)
+                                    config, connection_factory=DictConnection)
 
         if DBConnection.cur == None:
             # get a new cursor, if we don't have one
@@ -60,6 +64,7 @@ class Root(object):
 
 
 class EventIDs(DBConnection):
+    """Holds a list of ids for a ticket, which are queried at initialisation."""
     def __init__(self, ticket):
         super().__init__()
 
@@ -84,12 +89,68 @@ def get_eventids(ticket=""):
 def events_ids_info(self, request):
 
     # the request.after declarator adds a callback to manipulate the response
+    # only necessary if requests come from a different server:port as the app
     @request.after
     def manipulate_response(response):
         response.headers.add("Access-Control-Allow-Origin",
                              "http://localhost:8000")
 
     return self.ids
+
+class Events(DBConnection):
+    def __init__(self, ticket):
+        super().__init__()
+
+        self.ticket = ticket
+        self.ids = EventIDs(ticket).ids
+        self.events = []
+
+        try:
+            DBConnection.cur.execute(
+                    "SELECT * FROM events WHERE id = ANY(%s)", (self.ids,))
+
+            rows = DBConnection.cur.fetchall()
+            for row in rows:
+                # Remove None entries from the resulting dict.
+                # And transform datetime objects to string, because
+                # webob calls plain json_dumps which cannot serialize datetimes.
+                event = {k: (v.isoformat() if isinstance(v, datetime) else v)
+                         for k,v in row.items()
+                         if v != None
+                        }
+                self.events.append(event)
+        finally:
+            pass
+
+@App.path(model=Events, path='getEventsForTicket', required=['ticket'])
+def get_events(ticket=""):
+        return Events(ticket)
+
+@App.json(model=Events)
+def events_info(self, request):
+    @request.after
+    def manipulate_response(response):
+        response.headers.add("Access-Control-Allow-Origin",
+                             "http://localhost:8000")
+    return self.events
+
+
+class LastTicket(DBConnection):
+    def __init__(self):
+        super().__init__()
+        self.ticket = db.last_ticket_number(DBConnection.cur)
+
+@App.path(model=LastTicket, path='getLastTicketNumber')
+def get_last_ticket_number():
+        return LastTicket()
+
+@App.json(model=LastTicket)
+def events_info(self, request):
+    @request.after
+    def manipulate_response(response):
+        response.headers.add("Access-Control-Allow-Origin",
+                             "http://localhost:8000")
+    return self.ticket
 
 if __name__ == '__main__':
     morepath.run(App())
