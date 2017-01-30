6c02341589377976150c044854e432bc5cdebb25
Bernhard Herzog <bernhard.herzog@intevation.de>
2017-01-30 19:03:22 +0100

    Don't repeat column titles in shadowserver example script
    Instead of repeating the column titles in every CSV format defined in
    the shadowserver example script, we now use a mapping from attribute
    name to title and a helper function to assign those titles when creating
    all the table format objects.
    
    The main goal of this commit is to show how a script could avoid having
    to specify column titles multiple times.

--- example-scripts/10shadowservercsv.py
+++ example-scripts/10shadowservercsv.py
@@ -1,129 +1,182 @@
 from intelmqmail.tableformat import build_table_formats, ExtraColumn
 
 
+standard_column_titles = {
+    # column titles for standard event attributes
+    'classification.identifier': 'malware',
+    'destination.fqdn': 'dst_host',
+    'destination.ip': 'dst_ip',
+    'destination.port': 'dst_port',
+    'protocol.transport': 'proto',
+    'source.asn': 'asn',
+    'source.ip': 'ip',
+    'source.local_hostname': 'server_name',
+    'source.port': 'src_port',
+    'source.reverse_dns': 'hostname',
+    'time.source': 'timestamp',
 
-table_formats = build_table_formats([
+    # column titles for extra attributes
+    'extra:system_desc': 'sysdesc',
+    "extra:dns_hostname": "dns_host_name",
+    "extra:mssql_version": "version",
+    "extra:mongodb_version": "version",
+    "extra:visible_db_excerpt": "visible_databases",
+    "extra:workgroup_name": "workgroup",
+    "extra:machine_name": "machine_name",
+    "extra:elasticsearch_version": "version",
+    "extra:workstation_info": "workstation_info",
+    "extra:memcached_version": "version",
+    "extra:redis_version": "version",
+    "extra:ssdp_server": "server",
+    "extra:subject_common_name": "subject_common_name",
+    "extra:issuer_common_name": "issuer_common_name",
+    "extra:freak_cipher_suite": "freak_cipher_suite",
+}
+
+
+def add_default_titles(columns):
+    """Add the standard title to each the columns."""
+    extended_columns = []
+    for col in columns:
+        if isinstance(col, str):
+            title = standard_column_titles[col]
+            if col.startswith("extra:"):
+                extended_columns.append(ExtraColumn(col[6:], title))
+            else:
+                extended_columns.append((col, title))
+        else:
+            extended_columns.append(col)
+    return extended_columns
+
+
+def table_formats_with_default_titles(formats):
+    """Frontend for build_table_formats that adds standard column titles."""
+    return build_table_formats([(name, add_default_titles(columns))
+                                for name, columns in formats])
+
+
+table_formats = table_formats_with_default_titles([
     ("csv_malware", [
         # this is used for the following feeds:
         #   "Botnet-Drone-Hadoop", "Sinkhole-HTTP-Drone",
         #   "Microsoft-Sinkhole"
         # These names are all mapped to "csv_malware" by the rule expert bot
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ("classification.identifier", "malware"),
-        ("source.port", "src_port"),
-        ("destination.ip", "dst_ip"),
-        ("destination.port", "dst_port"),
-        ("destination.fqdn", "dst_host"),
-        ("protocol.transport", "proto"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "classification.identifier",
+        "source.port",
+        "destination.ip",
+        "destination.port",
+        "destination.fqdn",
+        "protocol.transport",
         ]),
     ("csv_DNS-open-resolvers", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
+        "source.asn",
+        "source.ip",
+        "time.source",
         ]),
     ("csv_Open-Portmapper", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
+        "source.asn",
+        "source.ip",
+        "time.source",
         ]),
     ("csv_Open-SNMP", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("system_desc", "sysdesc"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:system_desc",
         ]),
     ("csv_Open-LDAP", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("dns_hostname", "dns_host_name"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:dns_hostname",
         ]),
     ("csv_Open-MSSQL", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("mssql_version", "version"),
-        ("source.local_hostname", "server_name"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:mssql_version",
+        "source.local_hostname",
         ExtraColumn("instance_name", "instance_name"),
         ]),
     ("csv_Open-MongoDB", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("mongodb_version", "version"),
-        ExtraColumn("visible_db_excerpt", "visible_databases"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:mongodb_version",
+        "extra:visible_db_excerpt",
         ]),
     ("csv_Open-Chargen", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
+        "source.asn",
+        "source.ip",
+        "time.source",
         ]),
     ("csv_Open-IPMI", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
+        "source.asn",
+        "source.ip",
+        "time.source",
         ]),
     ("csv_Open-NetBIOS", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("workgroup_name", "workgroup"),
-        ExtraColumn("machine_name", "machine_name"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:workgroup_name",
+        "extra:machine_name",
         ]),
     ("csv_NTP-Monitor", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
+        "source.asn",
+        "source.ip",
+        "time.source",
         ]),
     ("csv_Open-Elasticsearch", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("elasticsearch_version", "version"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:elasticsearch_version",
         ExtraColumn("instance_name", "name"),
         ]),
     ("csv_Open-mDNS", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("workstation_info", "workstation_info"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:workstation_info",
         ]),
     ("csv_Open-Memcached", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("memcached_version", "version"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:memcached_version",
         ]),
     ("csv_Open-Redis", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("redis_version", "version"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:redis_version",
         ]),
     ("csv_Open-SSDP", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ExtraColumn("ssdp_server", "server"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "extra:ssdp_server",
         ]),
     ("csv_Ssl-Freak-Scan", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ("source.reverse_dns", "hostname"),
-        ExtraColumn("subject_common_name", "subject_common_name"),
-        ExtraColumn("issuer_common_name", "issuer_common_name"),
-        ExtraColumn("freak_cipher_suite", "freak_cipher_suite"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "source.reverse_dns",
+        "extra:subject_common_name",
+        "extra:issuer_common_name",
+        "extra:freak_cipher_suite",
         ]),
     ("csv_Ssl-Scan", [
-        ("source.asn", "asn"),
-        ("source.ip", "ip"),
-        ("time.source", "timestamp"),
-        ("source.reverse_dns", "hostname"),
-        ExtraColumn("subject_common_name", "subject_common_name"),
-        ExtraColumn("issuer_common_name", "issuer_common_name"),
+        "source.asn",
+        "source.ip",
+        "time.source",
+        "source.reverse_dns",
+        "extra:subject_common_name",
+        "extra:issuer_common_name",
         ]),
     ])
 
