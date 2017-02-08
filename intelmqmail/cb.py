c3fa0600c8d6340def21814978cf5218f543f12a
Dustin Demuth <dustin@intevation.de>
2017-02-08 09:17:28 +0100

    Changed Name of one CSV-Column in OpenLDAP Output

--- intelmqmail/cb.py
+++ intelmqmail/cb.py
@@ -230,7 +230,7 @@ feed_specific_formats = build_table_formats([
         ("source.asn", "asn"),
         ("source.ip", "ip"),
         ("time.source", "timestamp"),
-        ("source.local_hostname", "dns_host_name"),
+        ("source.local_hostname", "dns_hostname"),
         ]),
     ("Open-MSSQL", [
         ("source.asn", "asn"),
