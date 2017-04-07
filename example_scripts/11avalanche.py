0393d48fd631c0b51600407b9aa66de65c14e3ec
Dustin Demuth <dustin@intevation.de>
2017-04-07 18:16:56 +0200

    Made Scripts more consistent:
     * Both Shadowserver and Avalanche are now using the directives
       "notification_format" field to determine if a notification
       script shall work on the dataset.
    
    Requires: https://github.com/Intevation/intelmq/commit/9a68a6ce9a53ca4563f86126a462594b35e7fbff

--- example_scripts/11avalanche.py
+++ example_scripts/11avalanche.py
@@ -1,29 +1,33 @@
 from intelmqmail.tableformat import build_table_format
 import json
 
+
 def create_notifications(context):
-    # Read the substitutions from a file
-    js = {}
-    with open('/etc/intelmq/mailgen/formats/variables.json', 'r') as j:
-        js = json.load(j)
 
-    substitution_variables = js.get("substitutions")
-    if substitution_variables:
-        substitution_variables["ticket_prefix"] = js.get("common_strings").get("ticket_prefix")
+    if context.directive["notification_format"] == "avalanche":
 
-    ## Determine the kind of Aggregation.
-    aggregation = dict(context.directive["aggregate_identifier"])
-    asn_or_cidr = ""  # Can also be a CC
-    if "source.asn" in aggregation:
-        asn_or_cidr += "about AS %s" % aggregation["source.asn"]
-    elif "cidr" in aggregation:
-        asn_or_cidr = "about CIDR %s" % aggregation["cidr"]
-    elif "source.geolocation.cc" in aggregation:
-        asn_or_cidr = "about your Country %s" % aggregation["source.geolocation.cc"]
+        # Read the substitutions from a file
+        js = {}
+        with open('/etc/intelmq/mailgen/formats/variables.json', 'r') as j:
+            js = json.load(j)
 
-    substitution_variables["asn_or_cidr"] = asn_or_cidr
+        substitution_variables = js.get("substitutions")
+        if substitution_variables:
+            substitution_variables["ticket_prefix"] = js.get("common_strings").get("ticket_prefix")
 
-    if context.directive["event_data_format"] == "csv":
+        ## Determine the kind of Aggregation.
+        aggregation = dict(context.directive["aggregate_identifier"])
+        asn_or_cidr = ""  # Can also be a CC
+        if "source.asn" in aggregation:
+            asn_or_cidr += "about AS %s" % aggregation["source.asn"]
+        elif "cidr" in aggregation:
+            asn_or_cidr = "about CIDR %s" % aggregation["cidr"]
+        elif "source.geolocation.cc" in aggregation:
+            asn_or_cidr = "about your Country %s" % aggregation["source.geolocation.cc"]
+
+        substitution_variables["asn_or_cidr"] = asn_or_cidr
+
+        data_format = context.directive["event_data_format"]
         # Prepare Message in CSV-Format
         formats = (
             ("source.asn", "ASN"),
@@ -37,12 +41,14 @@ def create_notifications(context):
             ("destination.fqdn", "Destination-FQDN"))
 
         format = build_table_format("avalanche", formats)
-        if context.directive["notification_format"] == "inline":
+
+        if data_format == "avalanche_csv_inline":
             # If Inline-Messages are wanted
             return context.mail_format_as_csv(format,substitutions=substitution_variables)
-        if context.directive["notification_format"] == "attachment":
-            # If Attachment-Messages are wanted
-            # TODO: Call correct Method
-            return context.mail_format_as_csv(format,substitutions=substitution_variables)
+
+        elif data_format == "avalanche_csv_attachment":
+            # If Inline-Messages are wanted
+            # TODO There is no attachment, yet!
+            return context.mail_format_as_csv(format, substitutions=substitution_variables)
 
     return None
