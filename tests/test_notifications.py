303119433e4664fd28550553620507dc4676b1a5
Bernhard Herzog <bernhard.herzog@intevation.de>
2017-05-24 19:18:30 +0200

    Fix TestScriptContext test cases
    They weren't adapted to the recent introduction of the Directive class.

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -6,15 +6,27 @@ Tests for intelmqmail.notifications.
 import unittest
 from datetime import datetime, timedelta
 
-from intelmqmail.notification import ScriptContext
+from intelmqmail.notification import ScriptContext, Directive
 
 
 class TestScriptContext(unittest.TestCase):
 
-    def context_with_directive(self, last_sent=None,
-                               notification_interval=None):
-        directive = dict(last_sent=last_sent,
-                         notification_interval=notification_interval)
+    def context_with_directive(self, recipient_address="admin@example.com",
+                               template_name="generic_plaintext.txt",
+                               notification_format="generic_plaintext",
+                               event_data_format="inline_csv",
+                               aggregate_identifier=(),
+                               event_ids=(100001, 100302),
+                               directive_ids=(10, 11, 12), inserted_at=None,
+                               last_sent=None, notification_interval=None):
+        directive = Directive(recipient_address=recipient_address,
+                              template_name=template_name,
+                              notification_format=notification_format,
+                              event_data_format=event_data_format,
+                              aggregate_identifier=aggregate_identifier,
+                              event_ids=event_ids, directive_ids=directive_ids,
+                              inserted_at=inserted_at, last_sent=last_sent,
+                              notification_interval=notification_interval)
         return ScriptContext(None, None, None, directive, None)
 
     def test_notification_interval_exceeded_no_last_sent(self):
