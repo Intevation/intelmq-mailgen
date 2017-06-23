95448a6999228dd4fd0e2f7756186d842a6accb9
Bernhard Herzog <bernhard.herzog@intevation.de>
2017-06-23 17:01:35 +0200

    Remove web-API implementations
    They have been moved to intelmq-fody-backend a while ago.

--- extras/checkticket_api/checkticket_api/__main__.py
+++
@@ -1,8 +0,0 @@
-"""
-Trying a new pattern from
-https://moshez.wordpress.com/2016/06/07/__name__-__main__-considered-harmful/
-"""
-if __name__ != '__main__':
-        raise ImportError("This module cannot be imported.")
-from checkticket_api import serve
-serve.main()
