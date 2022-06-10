b3afd662b8bc66f342274eff6bce1fd9ec6fbbfd
Sebastian Wagner <swagner@intevation.de>
2022-06-10 19:54:35 +0200

    setup: cleanup and fixes
    fix classifiers
    remove obsolete imports and comments
    set the description content type, remove description from setup.py
    
    dependencies: remove python3.6 condition for gpg, lower versions of
    python are no longer in use anyway

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 # To use a consistent encoding
-from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='intelmqmail',
@@ -22,7 +17,6 @@ setup(
     version='1.3.0',
 
     description='A mail sending module for IntelMQ',
-    long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/intevation/intelmq-mailgen',
@@ -37,8 +31,8 @@ setup(
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU Affero General Public License v>=3.0',
-        'License :: OSI Approved :: GNU Lesser General Public License v>=2.1',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
 
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
@@ -54,7 +48,7 @@ setup(
 
     install_requires=[
         'psycopg2',
-        'gpg >= "1.10"; python_version >= "3.6"', # /!\ can (probably) **not** be installed via pip
+        'gpg >= 1.10',  # /!\ can (probably) **not** be installed via pip
         # Additional requirements:
         # * GnuPG (v>=2.2) for python3-gpg
         # * (optional) pyxarf v>0.0.5 for python3 installed
@@ -62,13 +56,6 @@ setup(
         #    (v==0.0.5 does **not** work)
         #    version 2502a80ae9178a1ba0b76106c800d0e4b779d8da shall work
     ],
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-#    package_data={
-#        'sample': ['package_data.dat'],
-#    },
 
     entry_points={
         'console_scripts': [
