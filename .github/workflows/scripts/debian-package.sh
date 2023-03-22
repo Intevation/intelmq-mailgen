13121a7c38a7fc879583a632c39f6006c3ef5c26
Sebastian Wagner <swagner@intevation.de>
2023-03-22 09:53:44 +0100

    ci: add missing debian build script

---
+++ .github/workflows/scripts/debian-package.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2023 Intevation GmbH
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+
+set -x
+set -e
+
+PARENT=$(dirname "${GITHUB_WORKSPACE}")
+echo "Building on ${codename} in ${GITHUB_WORKSPACE}"
+
+# install build dependencies
+# gpg can't be a package dependency because of https://bugs.launchpad.net/ubuntu/+source/gpgme1.0/+bug/1977645
+DEBIAN_FRONTEND="noninteractive" apt-get update -qq
+DEBIAN_FRONTEND="noninteractive" apt-get install python3-gpg dpkg-dev lintian -y
+DEBIAN_FRONTEND="noninteractive" apt-get build-dep -y .
+
+#chown -R nobody:nogroup "${PARENT}"
+dpkg-buildpackage -us -uc -b
