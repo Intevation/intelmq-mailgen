877478b2a13a6e2ecba678fb846b07f284617933
Sebastian Wagner <swagner@intevation.de>
2023-03-22 10:03:39 +0100

    ci: install packages with sudo

--- .github/workflows/scripts/debian-package.sh
+++ .github/workflows/scripts/debian-package.sh
@@ -11,9 +11,9 @@ echo "Building on ${codename} in ${GITHUB_WORKSPACE}"
 
 # install build dependencies
 # gpg can't be a package dependency because of https://bugs.launchpad.net/ubuntu/+source/gpgme1.0/+bug/1977645
-DEBIAN_FRONTEND="noninteractive" apt-get update -qq
-DEBIAN_FRONTEND="noninteractive" apt-get install python3-gpg dpkg-dev lintian -y
-DEBIAN_FRONTEND="noninteractive" apt-get build-dep -y .
+DEBIAN_FRONTEND="noninteractive" sudo -E apt-get update -qq
+DEBIAN_FRONTEND="noninteractive" sudo -E apt-get install python3-gpg dpkg-dev lintian -y
+DEBIAN_FRONTEND="noninteractive" sudo -E apt-get build-dep -y .
 
 #chown -R nobody:nogroup "${PARENT}"
 dpkg-buildpackage -us -uc -b
